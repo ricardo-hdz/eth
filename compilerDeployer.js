1505a315846d8e992c10e773cba9db6d52321610
Ricardo Hernandez <ricardo.a.hernandez@disney.com>
2018-04-03 16:17:19 -0700

    Custom payable token

--- compilerDeployer.js
+++ compilerDeployer.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const solc = require('solc');
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-var contractName = 'DogToken';
-var outputContractName = ':' + 'DogToken';
-var abiFilename = 'DogToken';
+var contractName = 'Mint';
+var outputContractName = ':' + 'Mint';
+var abiFilename = 'Mint';
 const input = fs.readFileSync( contractName + '.sol');
 const output = solc.compile({
 	sources: {
